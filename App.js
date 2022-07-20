c1c0ea718812b0193fdc8c30d6d90c1ec4a26e7a
Abel Kim <91298955+kdn0325@users.noreply.github.com>
2022-07-20 23:59:16 +0900

    RN-study

--- App.js
+++ App.js
@@ -15,4 +15,8 @@ const App = () => {
 // native에서는 flexDirection의 default 값은 Column
 
 //native에서는 width나 height값을 대부분 적용하지 않는다
+//native에서는 기본 default가 flexbox
+
+//flex의 비율 값
+
 export default App;
