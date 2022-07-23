b0bbbeee18d65525a94d1e2716720563befcb9ff
Abel Kim <91298955+kdn0325@users.noreply.github.com>
2022-07-23 18:26:32 +0900

    Rn

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, View ,Text} from 'react-native';
 
 const App = () => {
   return (
-    <View style={{flex:1}}>
-      <View style={{flex:1 ,backgroundColor:"red"}}></View>
-      <View style={{flex:1 ,backgroundColor:"green"}}></View>
-      <View style={{flex:1 ,backgroundColor:"blue"}}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text>Seoul</Text>
+      </View>
+      <View style={styles.weather}></View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor:"red",
+  },
+  city:{
+    flex:1,
+    backgroundColor:"blue",
+    justifyContent:"center",
+    alignItems:"center"
+  },
+  weather : {
+    flex:3,
+    backgroundColor:"green"
+  }
+})
 
 // Web에서는 flexDirection의 default 값은 Row
 // native에서는 flexDirection의 default 값은 Column
